b997e3e5c624fffbdf78edcfcfdc09f2ba0aef6c
Kai Sellgren <kaisellgren@gmail.com>
2019-03-22 18:00:56 +0200

    Update docs to display current page

--- packages/website/src/client/docs/docs-controller.js
+++ packages/website/src/client/docs/docs-controller.js
@@ -1,10 +1,13 @@
 import { createReactiveElement } from '@baseloop/core'
 import DocsView from './docs-view'
+import { map } from 'rxjs/operators'
 
 export default function DocsController (router) {
+  const page = router.onEnter('docs').pipe(map(v => v.pathVariables.page))
 
   return {
     view: createReactiveElement(DocsView, {
+      page,
       router: router.view,
     }),
   }
--- packages/website/src/client/docs/docs-view.js
+++ packages/website/src/client/docs/docs-view.js
@@ -3,15 +3,12 @@ import { Link } from '@baseloop/ui'
 
 export default class DocsView extends React.PureComponent {
   render () {
-    const {router} = this.props
+    const {router, page} = this.props
 
     return (
       <div>
         <h1>Docs</h1>
-        <Link router={router} routeName="docs.foo">Docs foo</Link>
-        <Link router={router} routeName="docs.bar">Docs bar</Link>
-        {router.match('docs.foo') && <div>foo</div>}
-        {router.match('docs.bar') && <div>bar</div>}
+        {page}
       </div>
     )
   }
