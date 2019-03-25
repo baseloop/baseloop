d7129fa7ffa31d52a7db3b055d314eca85222251
David Notik <dave@notik.com>
2019-03-24 21:51:54 -0700

    Fix smooth scrolling on iOS

--- packages/website/src/client/styles/global-style.js
+++ packages/website/src/client/styles/global-style.js
@@ -52,6 +52,8 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     flex: 1;
     box-sizing: border-box;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling: touch;
   }
   
   footer {
