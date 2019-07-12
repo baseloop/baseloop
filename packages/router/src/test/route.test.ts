b50f7ad28c00ac4a31e372b75bedcf760fa8ad30
Kai Sellgren <kaisellgren@gmail.com>
2019-07-12 21:02:05 +0300

    Fix tests

--- packages/router/src/test/route.test.ts
+++ packages/router/src/test/route.test.ts
@@ -16,7 +16,7 @@ test('route matching', () => {
 
 test('route parsing', () => {
   expect(new Route({ path: '/docs/:id/foo/:foo', name: '' }).parse('/docs/5/foo/10')).toEqual({ id: '5', foo: '10' })
-  expect(new Route({ path: '/docs/:id/foo/:foo', name: '' }).parse('/docs/5')).toEqual(null)
+  expect(new Route({ path: '/docs/:id/foo/:foo', name: '' }).parse('/docs/5')).toEqual({})
 
   expect(new Route({ path: '/encoding/:test', name: '' }).parse('/encoding/test%2F%20%5E%20%C2%B4%20ersdf')).toEqual({
     test: 'test/ ^ ´ ersdf'
