4939537556951a05873f700c3838845eabaad19e
Kai Sellgren <kaisellgren@gmail.com>
2019-07-12 21:00:32 +0300

    Make Jest work with TS

--- packages/router/babel.config.js
+++ packages/router/babel.config.js
@@ -7,6 +7,8 @@ module.exports = {
           node: 'current'
         }
       }
-    ]
-  ]
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread']
 }
