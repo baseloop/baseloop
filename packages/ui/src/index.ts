c5d88d9f74d238e83d41a84a1cf8e279cd69a36b
Kai Sellgren <kai.sellgren@op.fi>
2019-08-06 17:54:57 +0300

    Add data retrieval utility to UI package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,18 @@ export { default as Flex } from './layout/flex'
 export { default as resetStyle } from './styles/reset-style'
 export { default as PositionFluid } from './layout/position-fluid'
 export { useObservable } from './hooks'
+
+export function retrieveDataFromDOM<T>(selector: string): T | null {
+  const element = document.querySelector(selector)
+  if (element != null) {
+    const json = element.textContent
+    if (json != null) {
+      try {
+        return JSON.parse(json)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+  return null
+}
