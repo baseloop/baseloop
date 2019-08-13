d8c77424157b787916d67056cac83231518d6b59
Kai  Sellgren <kaisellgren@gmail.com>
2019-08-13 18:50:44 +0300

    Update Baseloop ascii logo

--- packages/dev/bin/util.ts
+++ packages/dev/bin/util.ts
@@ -2,10 +2,12 @@
 import chalk from 'chalk'
 import path from 'path'
 
-export const baseloopAsciiLogo = chalk`  {red ____}                 {green _}                   
- {red | __ )}  {cyan __ _} {magenta ___}  {yellow ___}{green | |} {red ___}   {blue ___}  {magenta _ __}  
- {red |  _ \\} {cyan / _\`} {magenta / __|}{yellow / _ \\} {green |}{red / _ \\} {blue / _ \\}{magenta | '_ \\} 
- {red | |_) |} {cyan (_|} {magenta \\__ \\}  {yellow __/} {green |} {red (_) |} {blue (_)} {magenta | |_) |}
- {red |____/} {cyan \\__,_}{magenta |___/}{yellow \\___|}{green _|}{red \\___/} {blue \\___/}{magenta | .__/} 
-                                    {magenta |_|}`
+export const baseloopAsciiLogo = chalk`
+   ____                     _                   
+  | __ )   __ _  ___   ___ | |  ___    ___   _ __  
+  |  _ \\  / _\`  / __| / _ \\  | / _ \\  / _ \\ | '_ \\ 
+  | |_) |  (_|  \\__ \\   __/  |  (_) |  (_)  | |_) |
+  |____/  \\__,_ |___/ \\___| _| \\___/  \\___/ | .__/ 
+                                            |_|
+`
 export const binPath = path.resolve(__dirname)
