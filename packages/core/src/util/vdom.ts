266975b587432d5df27a1b23561f28aededb9b06
Kai Sellgren <kaisellgren@gmail.com>
2020-03-03 18:43:48 +0200

    Make createReactiveElement to not use inner state

--- packages/core/src/util/vdom.ts
+++ packages/core/src/util/vdom.ts
@@ -1,68 +1,17 @@
-import { ComponentClass, createElement, FunctionComponent, ReactElement, Component } from 'react'
-import { Observable, Subscription } from 'rxjs'
-import { map, first } from 'rxjs/operators'
+import { ComponentClass, createElement, FunctionComponent, ReactElement } from 'react'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { combineObject, ObservableRecord } from './rxjs'
 
 type Element<T> = FunctionComponent<T> | ComponentClass<T>
 
-interface Props<T> {
-  element: Element<T>
-  initialProps: T
-  observableProps: Observable<T>
-}
-
-interface State<T> {
-  props: T
-}
-
-class ReactiveElement<T> extends Component<Props<T>, State<T>> {
-  private observer?: Subscription
-
-  public constructor(props: Props<T>) {
-    super(props)
-
-    this.state = {
-      props: props.initialProps
-    }
-  }
-
-  public componentDidMount() {
-    this.observer = this.props.observableProps.subscribe({
-      next: props => {
-        this.setState({ props })
-      },
-      error: e => {
-        console.error(e)
-      }
-    })
-  }
-
-  public componentWillUnmount() {
-    if (this.observer != null) {
-      this.observer.unsubscribe()
-    }
-  }
-
-  public render() {
-    return createElement(this.props.element, this.state.props)
-  }
-}
-
 /**
  * This is like React.createElement(X) (or <X />). The only difference is: this lets you pass Observables as props.
- * The result is a React element, which will automatically update its state based on the passed Observable props.
- * You can mix and match both Observable and non-Observable props.
+ * The result is a React element. You can mix and match both Observable and non-Observable props.
  */
 export function createReactiveElement<T>(
   element: Element<T>,
   propsAsObservables: ObservableRecord<T>
 ): Observable<ReactElement> {
-  const observableProps: Observable<T> = combineObject(propsAsObservables)
-  return observableProps.pipe(
-    first(),
-    map((initialProps: T) => {
-      // @ts-ignore
-      return createElement(ReactiveElement, { element, initialProps, observableProps })
-    })
-  )
+  return combineObject(propsAsObservables).pipe(map((props: T) => createElement(element, props)))
 }
